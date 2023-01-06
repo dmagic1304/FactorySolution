69a3393126d545a8cd40452ddd3362c59007ba4f
Dominik Magic <dominikmagic5@gmail.com>
2023-01-06 14:43:47 -0800

    WIP: working on adding a machine to an engineer

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -24,6 +24,7 @@ namespace Factory.Controllers
 
     public ActionResult Create()
     {      
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
       return View();
     }
 
