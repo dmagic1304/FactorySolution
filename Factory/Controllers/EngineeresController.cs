2163ab9728a384d2f4e1fd857ffd7ab646033533
Dominik Magic <dominikmagic5@gmail.com>
2023-01-06 10:59:35 -0800

    added engineeres index view

---
+++ Factory/Controllers/EngineeresController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineeresController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineeresController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+    
+  }
+}
