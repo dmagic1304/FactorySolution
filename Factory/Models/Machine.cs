bf01c93f678ac564d186db3ba0be1f33753d6492
Dominik Magic <dominikmagic5@gmail.com>
2023-01-06 10:02:01 -0800

    correction to Models to create many-to-many relationship

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,6 @@ namespace Factory.Models
     {
         public int MachineId { get; set; }
         public string Name { get; set; }
-        public List<Engineer> Engineers { get; set; }
+        public List<EngineerMachine> JointEntities { get; }
     }
 }
