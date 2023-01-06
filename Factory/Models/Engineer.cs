4236315f7f9d14ae463ac69bb0f9855c84288c00
Dominik Magic <dominikmagic5@gmail.com>
2023-01-06 13:51:42 -0800

    WIP: working on data validation

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -5,6 +5,8 @@ namespace Factory.Models
     public class Engineer
     {
         public int EngineerId { get; set; }
+
+        [Required(ErrorMessage = "The engineer's name can't be empty!")]
         public string Name { get; set; }
         public List<EngineerMachine> JointEntities { get; }
     }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -5,6 +5,8 @@ namespace Factory.Models
     public class Machine
     {
         public int MachineId { get; set; }
+
+        [Required(ErrorMessage = "The machine's name can't be empty!")]
         public string Name { get; set; }
         public List<EngineerMachine> JointEntities { get; }
     }
